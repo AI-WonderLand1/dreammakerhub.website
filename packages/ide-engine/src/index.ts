337b8d25cba43ce7133a000da9ebbb708f766096
wonderingtribe <b1161976969@gmail.com>
2026-04-13 19:22:13 -0500

    npm install

--- packages/ide-engine/src/index.ts
+++ packages/ide-engine/src/index.ts
@@ -1,3 +1,4 @@
 export { WebContainerManager, type FileNode } from './WebContainerManager';
 export { TerminalEmulator, WONDERSPACE_TERMINAL_THEME } from './TerminalEmulator';
 export { WebContainerPersistence, type ProjectSnapshot } from './WebContainerPersistence';
+export { ErrorScanner, type ErrorPattern } from './ErrorScanner';
