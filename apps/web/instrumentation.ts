ce0f1941643edeb5bdf72b529fe50d46c10d85c5
danksmoker42001 <danksmoker42001@gmail.com>
2026-07-05 10:27:35 +0000

    lint

--- apps/web/instrumentation.ts
+++ apps/web/instrumentation.ts
@@ -1,8 +1,7 @@
-import { startAetherGuardWorker } from '../../runners/aetherguardWorker';
-
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     try {
+      const { startAetherGuardWorker } = await import('../../runners/aetherguardWorker');
       await startAetherGuardWorker();
     } catch {
       // daemon start failure is non-fatal for the app
